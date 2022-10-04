203785442ecefe6a24d45c3cfa05a76c16ff5da2
Hugo G Machado <hugogontijomachado@gmail.com>
2022-10-04 02:03:04 -0300

    update

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,39 +1,66 @@
 import Image from "next/image"
 import styles from "../../styles/Pokemon.module.css"
+import { useRouter } from "next/router"
 
 export const getStaticPaths = async() => {
     const maxPokemons = 251
     const api = 'http://pokeapi.co/api/v2/pokemon'
     const res = await fetch(`${api}/?limit=${maxPokemons}`)
     const data = await res.json()
-
+    
     //params
     const paths = data.results.map((pokemon,index) => {
         return {
-            params: {pokemonId:(index+1).toString() },
+            params: {pokemonId:(index+1).toString()},
         }
     })
+    console.log("paths:", paths)
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     }
 }
 
 export const getStaticProps = async(context) => {
     const id = context.params.pokemonId
+    console.log(id)
     
     const res = await fetch(`http://pokeapi.co/api/v2/pokemon/${id}`)
 
     const data = await res.json()
 
     return {
-        props: { pokemon: data }
+        props: { pokemon: data, context:context.params }
     }
 
 }
 
-export default function Pokemon({pokemon}) {
+export default function Pokemon(props) {
+    const router = useRouter()
+    
+    const { pokemon, context } = props
+
+    /* o GetStaticPaths serve para retornar uma lista de parâmetros pra pre-renderização de páginas
+       estáticas no momento do build, onde cada item na lista será enviado como um parâmetro no contexto
+       para a função getStaticProps realizar as operações necessárias e renderizar página por página
+            fluxo: params do item[0] na lista de retorno de getStaticPaths --> entregue como contexto para
+            a função getStaticProps, que realiza as ações necessárias e chama esta função para renderizar 
+            o componente. O nº de páginas geradas será o tamanho da lista retornada.
+
+       - Quando usamos a saida fallback: false, as páginas listadas serão renderizadas e não será permitido ao
+       usuário tentar renderizar outras páginas com parâmetros não previstos no getStaticPaths, ou seja, a função
+       getStaticProps não será executada nunca pelo usuário
+       - Entretanto, se retornamos fallback:true em getSidePaths, permitiremos ao usuário utilizar parâmetros não 
+       previstos e a renderização será feita na hora. Isso acontece da seguinte maneira: esta função do componente
+       é chamada inicialmente; uma tela de carregamento pode ser renderizada ao invés do componente, caso o fallback seja
+       true. O fallback sendo true, após esta primeira renderização do componente, a função getStaticProps será executada
+       com o contexto contendo o parâmetro do usuário, ao finalizar irá executar esta função de renderização do componente,
+       agora com o fallback ja sendo false e com as novas props pra renderizar a nova página dinamicamente
+    */
+    if(router.isFallback){
+        return <div>Carregando...</div>
+    }
     return (
         <div className={styles.pokemon_container}>
             <h1 className={styles.title}>{pokemon.name}</h1>

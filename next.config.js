c6ab95595f8820a429b9c520206c5b6fe82b6773
Hugo G Machado <hugogontijomachado@gmail.com>
2022-10-04 01:10:34 -0300

    project commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "cdn.traction.one",
+      "raw.githubusercontent.com"
+    ]
+  },
+}
+
+module.exports = nextConfig
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from "../styles/About.module.css"
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o projeto</h1>
+            <p>balbablabalb blab lab lab lab lab aba bab ababalb abl a</p>
+            <Image 
+                src="/images/charizard.png"
+                width="300"
+                height="300"
+                alt="Charizard"
+            />
+        </div>
+    )
+}
